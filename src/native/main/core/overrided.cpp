2d11be43364d45cb72260a3f897c14973fd87524
Lamfi <37219244+Lamfi@users.noreply.github.com>
2022-05-24 13:59:24 +0300

    PupaDupaDate
    -migrated OverridedName to core/overrided.cpp
    -deleted empty .cpp
    -deleted code from vtable.cpp
    -added VtableHelper::getTop() - return original ;

---
+++ src/native/main/core/overrided.cpp
@@ -0,0 +1,29 @@
+#include <hook.h>
+#include <mod.h>
+#include <logger.h>
+#include <symbol.h>
+#include <innercore_callbacks.h>
+#include <nativejs.h>
+#include <vtable.h>
+#include <stl/string>
+#define stl std::__ndk1
+
+#include <innercore/common.h>
+
+#include "../shared_headers/OverridedName.h"
+
+void OverridedName::addNameForId(int id, std::string name){
+	names[id] = name;
+};
+
+stl::string OverridedName::getNameForId(int id){
+	return to_stl(names[id]);
+};
+
+bool OverridedName::isExist(int id){
+	if(names.count(id) > 0){
+		return true;
+	}else return false;
+};
+
+std::map<int, std::string> OverridedName::names;
--- src/native/main/vtable.cpp
+++ src/native/main/vtable.cpp
@@ -11,49 +11,35 @@
 #include <innercore/common.h>
 
 #include "shared_headers/VtableHelper.h"
-#include "shared_headers/OverridedName.h"
 
 VtableHelper::VtableHelper(void* a){
-		original = a;
-		vtable = *(void***) a;
+	original = a;
+	vtable = *(void***) a;
 };
 
 void** VtableHelper::get(){
-		return vtable;
+	return vtable;
 };
 
+void* VtableHelper::getTop(){
+	return original;
+}
 void VtableHelper::resize(){
-		const int new_size = 512;
-		const int offset = 4;
-		void** vtable_old = this->vtable;
-		vtable = (void**) malloc((new_size + offset) * sizeof(void*));
-		memcpy(this->vtable, vtable_old - offset, (new_size + offset) * sizeof(void*));
-		this->vtable += offset;
-		*(void***) this->original = vtable;
+	const int new_size = 512;
+	const int offset = 4;
+	void** vtable_old = this->vtable;
+	vtable = (void**) malloc((new_size + offset) * sizeof(void*));
+	memcpy(this->vtable, vtable_old - offset, (new_size + offset) * sizeof(void*));
+	this->vtable += offset;
+	*(void***) this->original = vtable;
 }	
 
 void VtableHelper::patch(const char* table, const char* symbol, void* func){
-		int index = getVtableOffset(table, symbol);
-		this->get()[index] = func;
+	int index = getVtableOffset(table, symbol);
+	this->get()[index] = func;
 };
 
 void* VtableHelper::getAdreess(const char* table, const char* symbol){
-		return SYMBOL(table, symbol);
+	return SYMBOL(table, symbol);
 };
-
-void OverridedName::addNameForId(int id, std::string name){
-		names[id] = name;
-};
-
-stl::string OverridedName::getNameForId(int id){
-		return to_stl(names[id]);
-};
-
-bool OverridedName::isExist(int id){
-		if(names.count(id) > 0){
-			return true;
-		}else return false;
-};
-
-std::map<int, std::string> OverridedName::names;
 
