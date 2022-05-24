7301916b52bda02eafab0df22ff5afa9860e44a1
Lamfi <37219244+Lamfi@users.noreply.github.com>
2022-05-24 15:45:13 +0300

    Фиксы
    added inner.jar to classespath

--- src/java/Boot/src/com/core/api/Boot.java
+++ src/java/Boot/src/com/core/api/Boot.java
@@ -3,13 +3,14 @@ package com.core.api;
 import java.util.HashMap;
 
 import com.zhekasmirnov.horizon.runtime.logger.Logger;
+import com.zhekasmirnov.innercore.
 import com.core.api.level.area.TickingAreasManager;
 
 class Boot {
     public static void boot(HashMap data) {
         
         Logger.debug("TEST_MOD", "Hello from Java");
-        
+
     }  
 }
  
--- src/java/Boot/src/com/core/api/NativeCallback.java
+++ src/java/Boot/src/com/core/api/NativeCallback.java
@@ -1,10 +1,12 @@
 package com.core.api;
 
+import com.zhekasmirnov.innercore.api.mod.adaptedscript.AdaptedScriptAPI.Item;
 import com.zhekasmirnov.innercore.api.runtime.Callback;
 
 public class NativeCallback {
 
     public static void onEnchant(int id){
         Callback.invokeCallback("OnEnchant",id);
+
     }
 }
