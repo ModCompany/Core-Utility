7be47356c60393eeebb3cbfe75aa78b72a23aa50
Lamfi <37219244+Lamfi@users.noreply.github.com>
2022-05-25 19:02:23 +0300

    MIGRATION
    -migrated few engine .cpp files from core to engine

--- src/native/main/core/mcpe/client/Options.cpp
+++ src/native/main/core/mcpe/client/Options.cpp
@@ -1,6 +1,40 @@
 #include <innercore_callbacks.h>
-#include <client/Options.h>
 #include <java.h>
+#include <nativejs.h>
+
+#include <stl/string>
+#define stl std::__ndk1
+
+#include <client/ClientIntance.h>
+#include <client/Options.h>
+#include <innercore/global_context.h>
+
+export(void, client_Options_setRenderMobInfoState, jboolean a){
+    Options* option = GlobalContext::getMinecraftClient()->getOptions();
+    option->setDevRenderMobInfoState(a);
+   // option->set
+}
+
+JS_MODULE_VERSION(Govno, 1);
+
+
+
+JS_EXPORT(Govno, debug, "V()",(JNIEnv* env){
+    Options* option = GlobalContext::getMinecraftClient()->getOptions();
+    option->setDevRenderMobInfoState(true);
+    option->setDevRenderBoundingBoxes(true);
+    option->setDevRenderPaths(true);
+
+});
+
+
+
+JS_EXPORT(Govno, setDebug, "V()",(JNIEnv* env, int debug){
+    Options* option = GlobalContext::getMinecraftClient()->getOptions();
+    option->setRenderDebug((DebugHudOptions) debug);
+
+});
+
 export(jint,mcpe_client_Options_getUiProfilePointer,jlong pointer) {
 	return (jint) ((int) ((Options*) pointer)->getUIProfile());
 }
